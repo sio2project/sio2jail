3b943df09de19ae749c7f2d7d58dc220885a49d0
teqwve <teqwve@gmail.com>
2018-10-07 22:28:29 +0200

    Specify default seccomp action in basePolicy

--- src/seccomp/policy/SyscallPolicy.h
+++ src/seccomp/policy/SyscallPolicy.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "seccomp/SeccompRule.h"
+#include "seccomp/action/SeccompAction.h"
 
 namespace s2j {
 namespace seccomp {
@@ -9,6 +10,19 @@ namespace policy {
 class SyscallPolicy {
 public:
     virtual const std::vector<SeccompRule>& getRules() const = 0;
+};
+
+class BaseSyscallPolicy : public SyscallPolicy {
+public:
+    BaseSyscallPolicy(std::shared_ptr<action::SeccompAction> defaultAction)
+        : defaultAction_(defaultAction) {}
+
+    std::shared_ptr<action::SeccompAction> getDefaultAction() const {
+        return defaultAction_;
+    }
+
+private:
+    std::shared_ptr<action::SeccompAction> defaultAction_;
 };
 
 }
