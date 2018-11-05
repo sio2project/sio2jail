dc0edb88f60bada1faf69ef306d19825609e8c09
teqwve <teqwve@gmail.com>
2018-11-05 22:54:40 +0100

    Add memory measurement test

--- test/src/leak-bss.c
+++ test/src/leak-bss.c
@@ -2,8 +2,6 @@
 
 unsigned char bss[64UL * 1024 * 1024];
 
-int main() {
-    int n = 0;
-    scanf("%d", &n);
-    return bss[n];
+int main(int argc, const char* argv[]) {
+    return bss[((unsigned long)&argc) % (64 * 1024 * 1024)];
 }
