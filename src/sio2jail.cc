c93a96331c93689111352aba20ec7cce50d051c5
A-dead-pixel <me@otsrv.net>
2023-04-01 20:06:36 +0200

    Reap zombies

--- src/sio2jail.cc
+++ src/sio2jail.cc
@@ -2,10 +2,13 @@
 #include "s2japp/Application.h"
 
 #include <iostream>
+#include <sys/wait.h>
 
 int main(int argc, const char* argv[]) {
     try {
-        return s2j::app::Application(argc, argv).main();
+        int ret = s2j::app::Application(argc, argv).main();
+        while (wait(NULL)>0){}
+        return ret;
     }
     catch (const std::exception& ex) {
         std::cerr << "Exception occurred: " << ex.what() << std::endl;
