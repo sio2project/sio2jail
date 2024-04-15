0c867017d1bfefbade287021e8f45dec02d6ace1
Mateusz Masiarz <matimasiarz@gmail.com>
2024-04-15 14:11:03 +0200

    Bump version

--- src/s2japp/ApplicationSettings.cc
+++ src/s2japp/ApplicationSettings.cc
@@ -84,7 +84,7 @@ private:
 namespace s2j {
 namespace app {
 
-const std::string ApplicationSettings::VERSION = "1.4.4";
+const std::string ApplicationSettings::VERSION = "1.5.0";
 
 const std::string ApplicationSettings::DESCRIPTION =
         "SIO2jail, a sandbox for programming contests.";
